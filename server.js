83b86c16a45169c5cb19c51c87cb4230c8af3ded
SSS1267_ <sss1267@lunaiz.com>
2024-11-08 12:40:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,23 +18,26 @@ app.use("/api/schedules", scheduleRoutes);
 app.use("/api/issues", issueRoutes); // 문의 라우트 추가
 
 const cors = require("cors");
-const allowedOrigins = [
-  "http://localhost:5173",           // 개발 서버 주소
-  "https://your-production-link.com"  // 실제 배포 URL
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // 요청의 출처가 allowedOrigins에 포함되어 있거나, undefined(예: 서버 간 요청)인 경우 허용
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // 자격 증명을 포함한 요청 허용 설정
-}));
+app.use(cors());
+// const allowedOrigins = [
+//   "http://localhost:5173",           // 개발 서버 주소
+//   "https://your-production-link.com"  // 실제 배포 URL
+// ];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // 요청의 출처가 allowedOrigins에 포함되어 있거나, undefined(예: 서버 간 요청)인 경우 허용
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true, // 자격 증명을 포함한 요청 허용 설정
+// }));
+
+app.options("*", cors()); // 모든 경로에 대해 OPTIONS 메서드를 허용
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
