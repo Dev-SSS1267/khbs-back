145f278f6451acce091aa96fd774e664c44cc635
SSS1267_ <sss1267@lunaiz.com>
2024-11-08 13:25:25 +0900

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error.message);
