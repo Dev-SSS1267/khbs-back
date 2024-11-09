049af916821a64aab633ddec5c89ca11e51167a2
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-09 14:04:55 +0900

    Refactor issueRoutes to include updateIssue functionality

--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -5,6 +5,7 @@ const {
   addIssue,
   addResponse,
   getIssue,
+  updateIssue,
 } = require("../controllers/issueController");
 
 // 모든 문의 목록 조회
@@ -18,5 +19,9 @@ router.get("/:id", getIssue);
 
 // 문의에 대한 답변 추가
 router.post("/:id/response", addResponse);
+
+
+// 문의 수정
+router.put("/:id", updateIssue);
 
 module.exports = router;
