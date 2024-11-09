168488eab2ab3fe08be629d5c9385ffc431a368d
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-09 15:14:26 +0900

    Refactor Notice and Schedule models to remove unique constraint on author field

--- models/Notice.js
+++ models/Notice.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const NoticeSchema = new mongoose.Schema({
-  author: { type: String, required: true },
+  author: { type: String },
   title: { type: String, required: true },
   content: { type: String, required: true },
   createDate: { type: Date, default: Date.now },
